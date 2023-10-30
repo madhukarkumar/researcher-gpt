9319bea24b7398c13214473b7e7318dad8e01b40
Madhukar Kumar <madhukar.kumar@gmail.com>
2023-10-29 18:16:10 -0700

    First commit

--- app.py
+++ app.py
@@ -145,11 +145,12 @@ system_message = SystemMessage(
             
             Please make sure you complete the objective above with the following rules:
             1/ You should do enough research to gather as much information as possible about the objective
-            2/ If there are url of relevant links & articles, you will scrape it to gather more information
-            3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
-            4/ You should not make things up, you should only write facts & data that you have gathered
-            5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
-            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
+            2/ If there are urls of relevant links and articles, you will scrape them to gather more information
+            3/ After scraping and searching, you should think "is there any new things I should search & scrape based on the data I collected to increase research quality?" If the answer is yes, continue; But don't do this more than 4 iteratins
+            4/ Your final response should be about 1000 words
+            5/ You should not make things up, you should only write facts and data that you have gathered
+            6/ In the final output, you should include all reference data and links to back up your research; You should include all reference data and links to back up your research
+            7/ In the final output, you should include all reference data and links to back up your research; You should include all reference data and links to back up your research"""
 )
 
 agent_kwargs = {
